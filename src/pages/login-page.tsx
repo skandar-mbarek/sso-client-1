bcc41725bc4eb476c85aadd900fe25758ba22781
Skandar M. <skandar.m@trvlcode.com>
2025-10-30 15:27:38 +0100

    update url sso front netlify

--- src/pages/login-page.tsx
+++ src/pages/login-page.tsx
@@ -5,7 +5,7 @@ const LoginPage: React.FC = () => {
 
 
     const handleLogin = () => {
-    window.location.href ='https://main-sso-front.vercel.app?clientId=client-app-1'
+    window.location.href ='https://main-sso-front.netlify.app?clientId=client-app-1'
     };
 
     return (
